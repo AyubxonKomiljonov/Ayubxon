2ea673a57ed341c07b2b31a0a0ea9010f14a1279
AyubxonKomiljonov <javohirimomaliyev360@gmail.com>
2023-05-10 10:10:05 +0500

    html va javascript fileni githubga joyladik

---
+++ app.js
@@ -0,0 +1,46 @@
+// alert("saytga muvaffaqqiyatli kirdingiz")
+
+// confirm("davom ettirasanmi ")
+
+// prompt("kodni kirit")
+
+// var ozgaruvchilar = 10;
+
+// var name = prompt("ismingizni kiriting");
+// var name1 = prompt("familiyangizni kiriting");
+// var name2 = prompt("yoshingizni  kiriting");
+
+// confirm(
+//   "Ismi: " + name + " " + "fomiliyangiz:" + name1 + " " + "yoshingiz:" + name2
+// );
+
+// let a = 59;
+// let b = 48;
+// confirm(a * b);
+
+
+
+
+
+
+
+
+
+
+// let result = (100+5) * 3;
+// console.log(result);
+// console.log(2**12)
+// let a = (2)
+// console.log(--a)
+
+
+// let number = (15)
+// console.log(number)
+
+let a = 20;
+let b = 20;
+console.log(a<=b)
+
+
+
+
